ec9e429f5eb7d6ec003fea7f436ba734f21e20cb
Christopher Hannah <christopher.hannah@me.com>
2017-03-22 10:14:17 +0000

    Initial Commit

---
+++ Command Wiki/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Command Wiki
+//
+//  Created by Christopher Hannah on 22/03/2017.
+//  Copyright © 2017 Christopher Hannah. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
