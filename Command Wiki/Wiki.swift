1cae6b1ac0ad7c7ef4e23ebcaa6442d6d084b115
Christopher Hannah <christopher.hannah@me.com>
2017-03-22 23:38:37 +0000

    readme

---
+++ Command Wiki/Wiki.swift
@@ -0,0 +1,67 @@
+//
+//  Wiki.swift
+//  Command Wiki
+//
+//  Created by Christopher Hannah on 22/03/2017.
+//  Copyright © 2017 Christopher Hannah. All rights reserved.
+//
+
+import Foundation
+
+struct WikiResult {
+    let title: String
+    let text: String
+    let url: String
+}
+
+class Wiki {
+    
+    func searchWikipedia(_ query: String) -> [WikiResult] {
+        var results: [WikiResult] = []
+        var complete = false
+        if (query != "") {
+            let disallowedCharacters = CharacterSet(charactersIn: "!*'();:@&=$,/\\?%#[]`~\"")
+            let spaceSafeQuery = query.replacingOccurrences(of: " ", with: "+")
+            let safeQuery = spaceSafeQuery.addingPercentEncoding(withAllowedCharacters: disallowedCharacters.inverted)!
+            let urlPath = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=\(safeQuery)"
+            let url: URL = URL(string: urlPath)!
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: url, completionHandler: {data, response, error -> Void in
+                
+                if error != nil {
+                    
+                } else {
+                    let jsonResult = JSON(data: data!)
+                    
+                    var titleArray = [String]()
+                    var descriptionArray = [String]()
+                    var urlArray = [String]()
+                    
+                    for (_,subJson):(String, JSON) in jsonResult[1] {
+                        titleArray.append(subJson.string!)
+                    }
+                    for (_,subJson):(String, JSON) in jsonResult[2] {
+                        descriptionArray.append(subJson.string!)
+                    }
+                    for (_,subJson):(String, JSON) in jsonResult[3] {
+                        urlArray.append(subJson.string!)
+                    }
+                    for i in 0..<titleArray.count {
+                        let result = WikiResult(title: titleArray[i], text: descriptionArray[i], url: urlArray[i])
+                        results.append(result)
+                    }
+                    complete = true
+                }
+            })
+            task.resume()
+            while (!complete) {
+                
+            }
+            return results
+        }
+        
+        return results
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# cwiki
+Wikipedia searching from the command line!
+
+## Installation
+(This probably isn't the best way, but here it goes)
+
+1. Download the latest version of cwiki - [https://github.com/chrishannah/cwiki/releases](https://github.com/chrishannah/cwiki/releases).
+2. Open up your terminal app of choice.
+3. Navigate to the folder containing the "cwiki" file.
+4. Add the executable permission to the file: `chmod +x cwiki`.
+5. Move the file to thhe usr/local/bin directory so it can be launched as a command: `mv qwiki /usr/local/bin`.
+
+Then you can run a wikipedia search by typing `cwiki` followed by your query (If it's a multi-word query you'll need to use speech marks.). 
+
+## Demo
+
+![Demo](http://i.imgur.com/yAmKbWo.gifv)
+
+
